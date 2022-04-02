659fade33a69518293025047f6003f3bfb0b1487
Deck Stream <tneswick2@verizon.net>
2022-04-01 21:52:39 -0700

    Employee tests passed

--- __tests__/Employee.test.js
+++ __tests__/Employee.test.js
@@ -4,7 +4,7 @@ const Employee = require('../lib/Employee')
 test('creates employee object', () => {
     const employee = new Employee('jeff', '1', 'jeff@live.com')
 
-    expect(employee.name).toBe('Jeff')
+    expect(employee.name).toBe('jeff')
     expect(employee.id).toBe(1)
     expect(employee.email).toBe('jeff@live.com')
 })
@@ -12,8 +12,8 @@ test('creates employee object', () => {
 test('returns employee name, employee id, employee email, and employee Role', () => {
     const employee = new Employee('jeff', '1', 'jeff@live.com')
 
-    expect(employee.getName()).toReturnWith('Jeff')
-    expect(employee.getId()).toReturnWith(1)
-    expect(employee.getEmail()).toReturnWith('jeff@live.com')
-    expect(employee.getRole()).toReturnWith('Employee')
+    expect(employee.getName()).toBe('Jeff')
+    expect(employee.getId()).toBe(1)
+    expect(employee.getEmail()).toBe('jeff@live.com')
+    expect(employee.getRole()).toBe('Employee')
 })
