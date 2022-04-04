a8feed5cc72c0b5cdc5af54017e5b85cef87b272
Deck Stream <tneswick2@verizon.net>
2022-04-03 23:17:52 -0700

    work update, index

--- index.js
+++ index.js
@@ -224,10 +224,80 @@ const askIntern = function() {
                             type: 'list',
                             name: 'next',
                             message: 'Please select an option from the list',
-                            choices: ['']
+                            choices: ['Input more Engineers', 'Input more Interns', 'Complete team and generate HTML']
+                        })
+                        .then(({ next }) => {
+                            if (next === 'Input more Engineers') {
+                                askEngineer()
+                            }
+                            else if (next === 'Input more Interns') {
+                                askIntern()
+                            }
+                            else {
+                                generateHTML()
+                            }
                         })
                     }
                 })
         }
     })
+}
+
+const getManager = function() {
+    return `
+    <div class="card col-2 m-5">
+        <div class="card-header fs-5 text-center">
+            ${managerArr[0].getName()}<br/>
+            <strong>Manager</strong>
+        </div>
+        <ul class="list-group list-group-flush">
+            <li class="list-group-item">ID: ${managerArr[0].getId()}</li>
+            <li class="list-group-item">Email: <a href="mailto: ${managerArr[0].getEmail()}">${managerArr[0].getEmail()}</a></li>
+            <li class="list-group-item">Office Number: ${managerArr[0].officeNumber}</li>
+        </ul>
+    </div>`
+}
+
+const getEngineers = function() {
+    let html = "" 
+    for (let i = 0; i < engineerArr.length; i++) {
+        const engineer =
+        `<div class="card col-2 m-5">
+            <div class="card-header fs-5 text-center">
+                ${engineerArr[i].getName()}<br/>
+                <strong>Engineer</strong>
+            </div>
+            <ul class="list-group list-group-flush">
+                <li class="list-group-item">ID: ${engineerArr[i].getId()}</li>
+                <li class="list-group-item">Email: <a href="mailto: ${engineerArr.getEmail()}">${engineerArr[i].getEmail()}</li>
+                <li class="list-group-item">GitHub: <a href="https://github.com/${engineerArr[i].github}">${engineerArr[i].github}</li>
+            </ul>
+        </div>\n`
+        html += engineer
+    }
+    return html;
+}
+
+const getInterns = function () {
+    let html = ""
+    for (let i = 0; i < internArr.length; i++) {
+        const intern = 
+        `<div class="card col-2 m-5">
+            <div class="card-header fs-5 text-center">
+                ${internArr[i].getName()}<br/>
+                <strong>Intern</strong>
+            </div>
+            <ul class="list-group list-group-flush">
+                <li class="list-group-item">ID: ${internArr[i].getId()}</li>
+                <li class="list-group-item">Email: <a href="mailto: ${internArr.getEmail()}">${internArr[i].getEmail()}</li>
+                <li class="list-group-item">School: ${internArr[i].school}</li>
+            </ul>
+        </div>\n`
+        html += intern        
+    }
+    return html;
+}
+
+const generateHTML = function() {
+    
 }
